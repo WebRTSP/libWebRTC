dc3a01038c36ef8b87bb7057e8a511e61595bc16
sergey radionov <rsatom@gmail.com>
2022-02-13 16:02:07 +0700

    publish fixed

--- 1.checkout.sh
+++ 1.checkout.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 # https://chromiumdash.appspot.com/branches
-NAME=m98
+VERSION_NAME=m98
 BRANCH=4758
 
 export PATH=$PATH:$PWD/depot_tools
 
 cd WebRTC.org/src/
 
-if [ "$(git rev-parse --abbrev-ref HEAD)" != "$NAME" ]; then
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "$VERSION_NAME" ]; then
 	echo Checking out "refs/remotes/branch-heads/$BRANCH"...
-	git checkout -b $NAME refs/remotes/branch-heads/$BRANCH
+	git checkout -b $VERSION_NAME refs/remotes/branch-heads/$BRANCH
 
 	echo Syncing...
 	gclient sync -D
--- 2.aar.build.sh
+++ 2.aar.build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -e
 
-./1.checkout.sh
-
-export PATH=$PATH:$PWD/depot_tools
+. ./1.checkout.sh
 
 cd WebRTC.org/src/
 
 echo Building .aar...
-./tools_webrtc/android/build_aar.py --output "../../Publish/publish/libwebrtc-$NAME.aar" --arch "armeabi-v7a" "arm64-v8a"
+./tools_webrtc/android/build_aar.py --output "../../Publish/publish/libwebrtc-$VERSION_NAME.aar" --arch "armeabi-v7a" "arm64-v8a"
 
 cd -
