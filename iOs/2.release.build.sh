8296d84dcc970f401a181835c93c460cb67d9a55
sergey radionov <rsatom@gmail.com>
2022-03-02 17:01:49 +0700

    basic GitHub release support added

--- iOs/2.release.build.sh
+++ iOs/2.release.build.sh
@@ -4,10 +4,12 @@
 
 export PATH=$PATH:$PWD/depot_tools
 
-cd WebRTC.org/src/
+ARM64_OUT=$PWD/release/arm64
+XCFRAMEWORK_OUT=$PWD/release/WebRTC-$LIBWEBRTC_VERSION_NAME.xcframework
 
-ARM64_OUT=../../release/arm64
-XCFRAMEWORK_OUT=../../release/WebRTC-$LIBWEBRTC_VERSION_NAME.xcframework
+rm -rf $XCFRAMEWORK_OUT
+
+cd WebRTC.org/src/
 
 gn gen $ARM64_OUT --args='treat_warnings_as_errors=false is_component_build=false is_debug=false target_cpu="arm64" enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
 ninja -C $ARM64_OUT framework_objc
