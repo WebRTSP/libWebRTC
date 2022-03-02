bf6f748d4a629f4b456d3480307403d24cd92b24
sergey radionov <rsatom@gmail.com>
2022-03-02 15:35:37 +0700

    iOs: generate .xcframework too

--- iOs/2.debug.build.sh
+++ iOs/2.debug.build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -e
 
-./1.checkout.sh
+. ./1.checkout.sh
 
 export PATH=$PATH:$PWD/depot_tools
 
 cd WebRTC.org/src/
 
-gn gen ../../debug/arm64 --args='treat_warnings_as_errors=false is_component_build=false is_debug=true target_cpu="arm64" enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
-ninja -C ../../debug/arm64 framework_objc
+ARM64_OUT=../../debug/arm64
+XCFRAMEWORK_OUT=../../debug/WebRTC-$LIBWEBRTC_VERSION_NAME.xcframework
+
+rm -rf $XCFRAMEWORK_OUT
+
+gn gen $ARM64_OUT --args='treat_warnings_as_errors=false is_component_build=false is_debug=true target_cpu="arm64" enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
+ninja -C $ARM64_OUT framework_objc
+
+xcodebuild -create-xcframework \
+	-framework $ARM64_OUT/WebRTC.framework \
+	-output $XCFRAMEWORK_OUT
 
 cd - > /dev/null
--- iOs/2.release.build.sh
+++ iOs/2.release.build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
-./1.checkout.sh
+. ./1.checkout.sh
 
 export PATH=$PATH:$PWD/depot_tools
 
 cd WebRTC.org/src/
 
-gn gen ../../release/arm64 --args='treat_warnings_as_errors=false is_component_build=false is_debug=false target_cpu="arm64" enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
-ninja -C ../../release/arm64 framework_objc
+ARM64_OUT=../../release/arm64
+XCFRAMEWORK_OUT=../../release/WebRTC-$LIBWEBRTC_VERSION_NAME.xcframework
+
+gn gen $ARM64_OUT --args='treat_warnings_as_errors=false is_component_build=false is_debug=false target_cpu="arm64" enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
+ninja -C $ARM64_OUT framework_objc
+
+xcodebuild -create-xcframework \
+	-framework $ARM64_OUT/WebRTC.framework \
+	-output $XCFRAMEWORK_OUT
 
 cd - > /dev/null
