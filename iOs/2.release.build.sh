6aa38d159bd59e059797699c9a07a98c88fe93a5
sergey radionov <rsatom@gmail.com>
2022-03-03 19:36:21 +0700

    iOs: fixed symbols export in the latest lib versions
    >=m89

--- iOs/2.release.build.sh
+++ iOs/2.release.build.sh
@@ -6,7 +6,7 @@ rm -rf $RELEASE_XCFRAMEWORK_PATH
 
 cd WebRTC.org/src/
 
-gn gen $RELEASE_ARM64_OUT --args='treat_warnings_as_errors=false is_component_build=false is_debug=false target_cpu="arm64" enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
+gn gen $RELEASE_ARM64_OUT --args='treat_warnings_as_errors=false is_component_build=false is_debug=false target_cpu="arm64" rtc_enable_symbol_export=true enable_dsyms=true rtc_include_tests=false ios_enable_code_signing=false target_os="ios" use_xcode_clang=true'
 ninja -C $RELEASE_ARM64_OUT framework_objc
 
 xcodebuild -create-xcframework \
