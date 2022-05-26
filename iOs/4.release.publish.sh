2c4861738f2b36ce3f0e808ed8a33587b9ecd43a
sergey radionov <rsatom@gmail.com>
2022-05-26 17:28:08 +0700

    gh release requies "notes"

--- iOs/4.release.publish.sh
+++ iOs/4.release.publish.sh
@@ -3,7 +3,7 @@
 . ./env.sh
 
 if [[ $LIBWEBRTC_VERSION_NAME == *patched ]]; then
-	gh release create $LIBWEBRTC_VERSION_NAME $RELEASE_XCFRAMEWORK_ZIP_PATH --target $(git rev-parse $LIBWEBRTC_VERSION_NAME) --title $LIBWEBRTC_VERSION_NAME
+	gh release create $LIBWEBRTC_VERSION_NAME $RELEASE_XCFRAMEWORK_ZIP_PATH --target $(git rev-parse $LIBWEBRTC_VERSION_NAME) --title $LIBWEBRTC_VERSION_NAME --notes ""
 else
 	gh release create $LIBWEBRTC_VERSION_NAME $RELEASE_XCFRAMEWORK_ZIP_PATH --target $(git rev-parse $LIBWEBRTC_VERSION_NAME) --title $LIBWEBRTC_VERSION_NAME --notes "branch-heads/$LIBWEBRTC_BRANCH"
 fi
